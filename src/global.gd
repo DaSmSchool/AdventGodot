bd7ec320508612f7bdad9279fff0fc457efb46d6
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2024-11-26 14:04:34 -0500

    add base code and part of 2020D1 solution

--- src/global.gd
+++
@@ -1,3 +0,0 @@
-func load_advent_input(year: int, day: int) -> String:
-	var fileContent = FileAccess.get_file_as_string("res://input/Y%dD%d" % [year, day])
-	return fileContent
---
+++ src/run_solutions.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var focusYear: int = 2020
+var focusDay: int = 1
+
+func _ready():
+	run_solution(focusYear, focusDay)
+
+func run_solution(year: int, day: int) -> void:
+	var adventInput = Helper.load_advent_input(year, day)
+	print(get_node("%d" % focusYear).get_node("Day%d" % focusDay).solve(adventInput))
