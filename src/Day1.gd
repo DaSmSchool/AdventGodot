d6e1b67a8041454745d490567542dc940dbc19e2
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2024-11-26 14:04:34 -0500

    add base code and part of 2020D1 solution

--- src/Day1.gd
+++ src/Day1.gd
@@ -1,5 +1,4 @@
-extends Node
-
+extends Solution
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +8,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func solve(input: String) -> int:
+	var solution: int = 0
+	var splitInput: PackedStringArray = input.split("\n")
+	var num1: String
+	var num2: String
+	
+	for line1 in splitInput:
+		for line2 in splitInput:
+			if line1.hash() == line2.hash():
+				if line1.to_int() == 1010:
+					num1 = line1
+					num2 = line2
+					break
+				continue
+			
+	
+	return solution
---
+++ src/solution.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name Solution
+
+func solve(input: String) -> int:
+	var solution: int = 0
+	
+	return solution
