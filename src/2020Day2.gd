c863b877b008abe674ed88b46cf26875b57eae59
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2024-11-27 15:34:57 -0500

    add day 2 part 1

---
+++ src/2020Day2.gd
@@ -0,0 +1,39 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func solve1(input: String) -> int:
+	var solution: int = 0
+	
+	var inputLines: PackedStringArray = input.split("\n")
+	
+	for line: String in inputLines:
+		if line == "": continue
+		var inputSet: PackedStringArray = line.split(" ")
+		var policySet: PackedStringArray = inputSet[0].split("-")
+		var minCount: int = policySet[0].to_int()
+		var maxCount: int = policySet[1].to_int()
+		var selectedChar: String = inputSet[1][0]
+		var possiblePassword: String = inputSet[2]
+		
+		var appearCount: int = 0
+		for char in possiblePassword:
+			if char == selectedChar: appearCount += 1
+		
+		if appearCount <= maxCount and appearCount >= minCount: solution += 1
+		
+	return solution
+
+func solve2(input: String) -> int:
+	var solution: int = 0
+	
+	return solution
