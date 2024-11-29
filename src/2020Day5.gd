a47dca21ba134ab6e6e9e17ea61658c175fe3790
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2024-11-28 23:52:17 -0500

    add day 5 solution

--- src/2020Day5.gd
+++ src/2020Day5.gd
@@ -9,3 +9,59 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func solve1(input: String) -> int:
+	var seatIds: Array[int] = []
+	
+	var solution: int = 0
+	
+	var inputLines: PackedStringArray = input.split("\n")
+	
+	for line in inputLines:
+		if line == "": continue
+		var row: int = 0
+		for i in range(7):
+			if line[6-i] == "B":
+				row += pow(2, i)
+		
+		var seat: int = 0
+		for i in range(3):
+			if line[9-i] == "R":
+				seat += pow(2, i)
+		
+		seatIds.push_front(row*8 + seat)
+	
+	seatIds.sort()
+	
+	solution = seatIds.max()	
+	return solution
+
+
+func solve2(input: String) -> int:
+	var seatIds: Array[int] = []
+	
+	var solution: int = 0
+	
+	var inputLines: PackedStringArray = input.split("\n")
+	
+	for line in inputLines:
+		if line == "": continue
+		var row: int = 0
+		for i in range(7):
+			if line[6-i] == "B":
+				row += pow(2, i)
+		
+		var seat: int = 0
+		for i in range(3):
+			if line[9-i] == "R":
+				seat += pow(2, i)
+		
+		seatIds.push_front(row*8 + seat)
+	
+	seatIds.sort()
+	print(seatIds)
+	
+	for id in range(1, seatIds.size()-1):
+		if (seatIds[id+1] - seatIds[id-1]) == 3: solution = seatIds[id]-1
+	
+	return solution
