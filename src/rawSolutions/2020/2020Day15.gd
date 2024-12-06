a479163ef859ae8f0fd4453875757a0cbc32a8c9
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2024-12-05 22:57:40 -0500

    add 2020 day 15 p1 progress

--- src/rawSolutions/2020/2020Day15.gd
+++ src/rawSolutions/2020/2020Day15.gd
@@ -10,9 +10,26 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+var currentNum: int = 0
+var timesSaid: int = 0
+var saidIndex: Dictionary = {}
+
+func said_to_index() -> void:
+	if saidIndex.has(currentNum):
+		saidIndex[currentNum].push_back(timesSaid)
+	else:
+		saidIndex.get_or_add(currentNum, [timesSaid])
+
+func say_number(num: int) -> void:
+	currentNum = num
+	timesSaid += 1
+	said_to_index()
 
 func solve1(input: String) -> int:
 	var solution: int = 0
+	
+	var startingNums: PackedStringArray = input.split(",")
+	
 	
 	return solution
 
