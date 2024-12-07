feb69db63cac68384c24778bf90d423ca6681a7f
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2024-12-07 13:43:34 -0500

    add elapsed time to prints

--- src/helper.gd
+++ src/helper.gd
@@ -1,13 +1,16 @@
 extends Node
 class_name Helper
 
+static func print_fail(message: String) -> void:
+	print_rich("[color=red][shake rate=20.0 level=5 connected=1]%d[/shake][/color]" % message)
+
 static func load_advent_input(year: int, day: int) -> String:
 	var fileContent: String = FileAccess.get_file_as_string("res://input/%d/D%d.txt" % [year, day])
 	var fileError: int = FileAccess.get_open_error()
 	print_rich("[rainbow freq=0.2 sat=1.0 val=1.0][wave amp=50.0 freq=5.0 connected=1][color=green]YEAR %d | DAY %d[/color][/wave][/rainbow]" % [year, day])
 	if fileError != 0:
 		if fileError == 7:
-			print_rich("[color=red][shake rate=20.0 level=5 connected=1]ERROR: INPUT NOT FOUND![/shake][/color]")
+			print_fail("ERROR: INPUT NOT FOUND!")
 		else:
-			print_rich("[color=red][shake rate=20.0 level=5 connected=1]FAILED TO LOAD INPUT: ERROR" + str(fileError) + "[/shake][/color]")
+			print_fail("FAILED TO LOAD INPUT: ERROR" + str(fileError))
 	return fileContent
--- src/run_solutions.gd
+++ src/run_solutions.gd
@@ -8,5 +8,35 @@ func _ready():
 
 func run_solution(year: int, day: int) -> void:
 	var adventInput = Helper.load_advent_input(year, day)
-	print(get_node("%d" % focusYear).get_node("Day%d" % focusDay).solve1(adventInput))
-	print(get_node("%d" % focusYear).get_node("Day%d" % focusDay).solve2(adventInput))
+	
+	var microTime: int
+	var postMicroTime: int
+	var p1Sol: int
+	var p2Sol: int
+	var solNode: Solution = get_node("%d" % focusYear).get_node("Day%d" % focusDay)
+	if solNode == null:
+		Helper.print_fail("NODE FOR YEAR%d DAY%d NOT FOUND" % [year, day])
+	
+	# p1 run
+	microTime = Time.get_ticks_msec()
+	p1Sol = solNode.solve1(adventInput)
+	postMicroTime = Time.get_ticks_msec()
+	
+	var p1Elapsed: float = postMicroTime - microTime
+	print("PART 1 SOLUTION: %d" % [p1Sol])
+	print("TIME TAKEN:")
+	print("MILLISECONDS: %dms" % [p1Elapsed])
+	print("SECONDS: %d seconds" % [p1Elapsed/1000])
+	print()
+	
+	# p2 run
+	microTime = Time.get_ticks_msec()
+	p2Sol = solNode.solve2(adventInput)
+	postMicroTime = Time.get_ticks_msec()
+	
+	var p2Elapsed: float = postMicroTime - microTime
+	print("PART 2 SOLUTION: %d" % [p2Sol])
+	print("TIME TAKEN:")
+	print("MILLISECONDS: %dms" % [p2Elapsed])
+	print("SECONDS: %d seconds" % [p2Elapsed/1000])
+	print()
