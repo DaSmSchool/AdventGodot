ca6b9f18a3e1d43785c737c95654de1e8ff613b6
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2024-12-09 14:06:25 -0500

    add 2024 day 9 part 1 sol, part 2 progress

---
+++ src/rawSolutions/2024/2024Day9.gd
@@ -0,0 +1,105 @@
+extends Solution
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func solve1(input: String) -> int:
+	var solution: int = 0
+	
+	var assemble: Array[int] = []
+	
+	var empty: bool = false
+	var addNum: int = 0
+	for char: String in input:
+		if char == "\n": continue
+		if !empty:
+			for amount: int in char.to_int():
+				assemble.append(addNum)
+			addNum += 1
+		else:
+			for amount: int in char.to_int():
+				assemble.append(-1)
+		empty = !empty
+	
+	var forwardInd: int = 0
+	var backwardInd: int = assemble.size()-1
+	
+	#print(assemble)
+	
+	while forwardInd < backwardInd:
+		if assemble[forwardInd] == -1:
+			if assemble[backwardInd] != -1:
+				assemble[forwardInd] = assemble[backwardInd]
+				assemble[backwardInd] = -1
+			else:
+				backwardInd -= 1
+		else:
+			forwardInd += 1
+	
+	assemble
+	
+	for charInd: int in assemble.size():
+		if assemble[charInd] != -1:
+			solution += charInd * assemble[charInd]
+	
+	return solution
+
+
+func solve2(input: String) -> int:
+	var solution: int = 0
+	
+	var assemble: Array[int] = []
+	
+	var empty: bool = false
+	var addNum: int = 0
+	for char: String in input:
+		if char == "\n": continue
+		if !empty:
+			for amount: int in char.to_int():
+				assemble.append(addNum)
+			addNum += 1
+		else:
+			for amount: int in char.to_int():
+				assemble.append(-1)
+		empty = !empty
+	
+	var focusNum: int = assemble.max()
+	while focusNum >= 0:
+		var numEndInd: int = assemble.rfind(focusNum)
+		var extentInd = 0
+		var numAmount = 0
+		while assemble[extentInd] == focusNum:
+			numAmount += 1
+			extentInd -= 1
+		
+		var forwardSearch: int = 0
+		while assemble[forwardSearch] != focusNum:
+			if assemble[forwardSearch] == -1:
+				var emptyExtentInd = 0
+				var emptyNumAmount = 0
+				while assemble[forwardSearch+emptyExtentInd] == -1:
+					emptyNumAmount += 1
+					emptyExtentInd -= 1
+				if emptyNumAmount >= numAmount:
+					for numOff: int in numAmount:
+						assemble[forwardSearch+numOff] = focusNum
+						assemble[numEndInd-numAmount+numOff] = -1
+			forwardSearch += 1
+		
+		focusNum -= 1
+	
+	print(assemble)
+	
+	for charInd: int in assemble.size():
+		if assemble[charInd] != -1:
+			solution += charInd * assemble[charInd]
+		
+	return solution
