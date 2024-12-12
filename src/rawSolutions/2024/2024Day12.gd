dbf6635b665f4a14afa61c074e22ef211e29410f
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2024-12-12 14:43:05 -0500

    add 2024 day 12 progress

--- src/rawSolutions/2024/2024Day12.gd
+++ src/rawSolutions/2024/2024Day12.gd
@@ -18,14 +18,28 @@ func solve1(input: String) -> int:
 	var grid: Array = []
 	var untrackedInds: Dictionary = {}
 	
-	
 	for lineInd: int in splitInput.size():
 		if splitInput[lineInd] == "": continue
 		for charInd: int in splitInput[lineInd].length():
 			grid.append(splitInput[lineInd][charInd])
 			untrackedInds.get_or_add([charInd, lineInd])
 	
+	var groupsMade: Dictionary = {}
+	var gardenIndLists: Array = []
 	
+	for lineInd: int in grid.size():
+		var assignGroup: String = grid[lineInd][0]
+		if groupsMade.has(assignGroup):
+			groupsMade[assignGroup].append(groupsMade[assignGroup].back()+1)
+		else:
+			groupsMade.get_or_add(assignGroup, [0])
+		for charInd in grid[0].size():
+			var focusChar = grid[lineInd][charInd]
+			if focusChar != assignGroup:
+				
+			
+			if lineInd == 0:
+				if charInd !=
 	
 	
 	
