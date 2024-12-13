2039c1850915882d1067ded8b96edd7a40d101fc
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2024-12-12 21:30:19 -0500

    add 2024 day 12 part 1 solution

--- src/rawSolutions/2024/2024Day12.gd
+++ src/rawSolutions/2024/2024Day12.gd
@@ -13,7 +13,9 @@ func _process(delta: float) -> void:
 
 func get_group(yPos: int, xPos: int, grid: Array) -> Array:
 	var returnGrid: Array = []
-	var grabbedGrid: Array[Vector2i] = [Vector2i(xPos, yPos)]
+	var selfSpot: Vector2i  = Vector2i(xPos, yPos)
+	var grabbedGrid: Array[Vector2i] = [selfSpot]
+	returnGrid.append(selfSpot)
 	
 	while grabbedGrid.size() != 0:
 		var focusPoints: Array[Vector2i] = grabbedGrid.duplicate(true)
@@ -22,8 +24,9 @@ func get_group(yPos: int, xPos: int, grid: Array) -> Array:
 			for yInd: int in range(-1, 2, 1):
 				for xInd: int in range(-1, 2, 1):
 					if abs(yInd) != abs(xInd) and Helper.valid_pos_at_grid(point.y+yInd, point.x+xInd, grid):
-						if grid[point.y][point.x] == grid[point.y+yInd][point.x+xInd]:
-							grabbedGrid.append(Vector2(point.x+xInd, point.y+yInd))
+						var focusPoint: Vector2i = Vector2i(point.x+xInd, point.y+yInd)
+						if grid[point.y][point.x] == grid[point.y+yInd][point.x+xInd] and !returnGrid.has(focusPoint) and !grabbedGrid.has(focusPoint):
+							grabbedGrid.append(Vector2i(point.x+xInd, point.y+yInd))
 
 		returnGrid.append_array(grabbedGrid)
 	
@@ -34,27 +37,55 @@ func solve1(input: String) -> int:
 	
 	var splitInput: PackedStringArray = input.split("\n")
 	var grid: Array = []
-	var untrackedInds: Dictionary = {}
 	
-	for lineInd: int in splitInput.size():
-		if splitInput[lineInd] == "": continue
-		for charInd: int in splitInput[lineInd].length():
-			grid.append(splitInput[lineInd][charInd])
-			untrackedInds.get_or_add([charInd, lineInd])
+	for line: String in splitInput:
+		if line == "": continue
+		grid.append(line.split(""))
 	
 	var groupsMade: Dictionary = {}
-	var gardenIndLists: Array = []
 	
 	var assignGroup: String = grid[0][0]
-	groupsMade
+	groupsMade.get_or_add(assignGroup, [])
+	groupsMade[assignGroup].append(get_group(0, 0, grid))
 	
 	for lineInd: int in grid.size():
-		for charInd in grid[0].size():
+		for charInd: int in grid[0].size():
+			var charAtGrid: String = grid[lineInd][charInd]
+			if Helper.valid_pos_at_grid(lineInd, charInd-1, grid) and charAtGrid == grid[lineInd][charInd-1]:
+				continue
+			if charInd == 0 or (Helper.valid_pos_at_grid(lineInd, charInd, grid) and charAtGrid != grid[lineInd][charInd-1]):
+				if !groupsMade.has(charAtGrid):
+					groupsMade.get_or_add(charAtGrid, [])
+					groupsMade[charAtGrid].append(get_group(lineInd, charInd, grid))
+				else:
+					var hasPosition: bool = false
+					for arrayKey: String in groupsMade.keys():
+						for array: Array in groupsMade[arrayKey]:
+							if array.has(Vector2i(charInd, lineInd)):
+								hasPosition = true
+								break
+						if hasPosition: break
+					if hasPosition:
+						pass
+					else:
+						groupsMade[charAtGrid].append(get_group(lineInd, charInd, grid))
+	
+	for key: String in groupsMade.keys():
+		for garden: Array in groupsMade[key]:
+			var gardenArea: int = 0
+			var gardenPerimeter: int = 0
+			for indGarden: Vector2i in garden:
+				gardenArea += 1
+				for yOff: int in range(-1, 2, 1):
+					for xOff: int in range(-1, 2, 1):
+						if abs(yOff) != abs(xOff):
+							if !Helper.valid_pos_at_grid(indGarden.y+yOff, indGarden.x+xOff, grid):
+								gardenPerimeter += 1
+							else:
+								if grid[indGarden.y][indGarden.x] != grid[indGarden.y+yOff][indGarden.x+xOff]:
+									gardenPerimeter += 1
+			solution += gardenArea * gardenPerimeter
 			
-			
-	
-	
-	
 	return solution
 
 
