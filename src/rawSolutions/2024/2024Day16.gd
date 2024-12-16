c5878f92a06cd7eb161925d70a0b03a4b91ad67d
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2024-12-16 14:06:31 -0500

    Update 2024Day16.gd

--- src/rawSolutions/2024/2024Day16.gd
+++ src/rawSolutions/2024/2024Day16.gd
@@ -14,6 +14,25 @@ func _process(delta: float) -> void:
 func solve1(input: String) -> int:
 	var solution: int = 0
 	
+	var grid: Array = []
+	
+	for row in input.split(""):
+		if row == "": continue
+		grid.append(row.split(""))
+	
+	var unvisitedSpots: Dictionary = {}
+	var reindeerStart: Vector2i = Vector2i()
+	var finishPos: Vector2i = Vector2i()
+	
+	for rowInd: int in grid.size():
+		for charInd: int in grid[rowInd].size():
+			if grid[rowInd][charInd] == ".":
+				unvisitedSpots.get_or_add(Vector2i(charInd, rowInd), INF)
+			elif grid[rowInd][charInd] == "S":
+				reindeerStart = Vector2i(charInd, rowInd)
+			elif grid[rowInd][charInd] == "E":
+				finishPos = Vector2i(charInd, rowInd)
+	
 	
 	
 	return solution
