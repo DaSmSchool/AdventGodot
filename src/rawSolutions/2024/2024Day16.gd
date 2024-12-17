011122ff547cf52138004b7ddba86de3dc26da02
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2024-12-16 20:50:57 -0500

    add 2024 day 16 progress

--- src/rawSolutions/2024/2024Day16.gd
+++ src/rawSolutions/2024/2024Day16.gd
@@ -11,29 +11,93 @@ func _process(delta: float) -> void:
 	pass
 
 
+func get_path_length(path: String) -> int:
+	var pathLength: int = 0
+	
+	for charInd: int in path.length():
+		if charInd == 0: pass
+		else:
+			if path[charInd] != path[charInd-1]: pathLength += 1000
+			pathLength += 1
+	
+	return pathLength
+
+
 func solve1(input: String) -> int:
 	var solution: int = 0
 	
 	var grid: Array = []
 	
-	for row in input.split(""):
+	for row in input.split("\n"):
 		if row == "": continue
 		grid.append(row.split(""))
 	
 	var unvisitedSpots: Dictionary = {}
-	var reindeerStart: Vector2i = Vector2i()
+	var visitedSpots: Dictionary = {}
+	var startPos: Vector2i = Vector2i()
 	var finishPos: Vector2i = Vector2i()
 	
 	for rowInd: int in grid.size():
 		for charInd: int in grid[rowInd].size():
+			if unvisitedSpots.has(Vector2i(charInd, rowInd)): continue
 			if grid[rowInd][charInd] == ".":
 				unvisitedSpots.get_or_add(Vector2i(charInd, rowInd), INF)
 			elif grid[rowInd][charInd] == "S":
-				reindeerStart = Vector2i(charInd, rowInd)
+				startPos = Vector2i(charInd, rowInd)
+				visitedSpots.get_or_add(Vector2i(charInd, rowInd), ">")
 			elif grid[rowInd][charInd] == "E":
+				grid[rowInd][charInd] = "."
 				finishPos = Vector2i(charInd, rowInd)
+				unvisitedSpots.get_or_add(Vector2i(charInd, rowInd), INF)
 	
+	var hasSearchable: bool = true
 	
+	var possiblePaths: PackedStringArray = []
+	var spotsToVisit: Array = []
+	spotsToVisit.append([startPos, ""])
+	
+	while spotsToVisit.size() != 0:
+		
+		var focusSpot: Array
+		if spotsToVisit.size() == 1:
+			focusSpot = spotsToVisit[0]
+		else: 
+			focusSpot = spotsToVisit.reduce(func(min, spot): return spot if get_path_length(spot[1]) < get_path_length(min[1]) else min)
+		
+		#print(spotsToVisit)
+		#print(focusSpot)
+		
+		if focusSpot[0] == finishPos:
+			possiblePaths.append(focusSpot[1])
+			spotsToVisit.erase(focusSpot)
+			continue
+		
+		for yOff: int in range(-1, 2):
+			for xOff: int in range(-1, 2):
+				var offsetSpot: Vector2i = Vector2i(focusSpot[0].x + xOff, focusSpot[0].y + yOff)
+				var dirVec: Vector2i = Vector2i(xOff, yOff)
+				if abs(yOff) != abs(xOff) and ".E".contains(grid[offsetSpot.y][offsetSpot.x]) and unvisitedSpots.has(offsetSpot) and !visitedSpots.has(offsetSpot):
+					var visitSpotPath: String = String(focusSpot[1])
+					if dirVec == Vector2i.UP:
+						visitSpotPath += "^"
+					elif dirVec == Vector2i.DOWN:
+						visitSpotPath += "v"
+					elif dirVec == Vector2i.LEFT:
+						visitSpotPath += "<"
+					elif dirVec == Vector2i.RIGHT:
+						visitSpotPath += ">"
+					
+					spotsToVisit.append([offsetSpot, visitSpotPath])
+					
+					if grid[offsetSpot.y][offsetSpot.x] == ".":
+						unvisitedSpots.erase(offsetSpot)
+						visitedSpots.get_or_add(offsetSpot, visitSpotPath)
+				
+		spotsToVisit.erase(focusSpot)
+	
+	for path: String in possiblePaths:
+		print(">"+path)
+		print(get_path_length(">"+path))
 	
 	return solution
 
