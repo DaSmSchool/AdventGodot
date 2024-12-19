b4197c352610beb7bac823bbebf53d54119f508f
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2024-12-18 20:39:51 -0500

    add progress before path re-search

--- src/rawSolutions/2024/2024Day16.gd
+++ src/rawSolutions/2024/2024Day16.gd
@@ -34,6 +34,22 @@ func get_path_length_no_turn(path: String) -> int:
 	return pathLength
 
 
+func increment_path_list(pathArray: Array, dir: Vector2i) -> Array:
+	var assemble: Array = pathArray.duplicate(true)
+	
+	for strInd: int in assemble.size():
+		if dir == Vector2i.UP:
+			assemble[strInd] += "^"
+		elif dir == Vector2i.DOWN:
+			assemble[strInd] += "v"
+		elif dir == Vector2i.LEFT:
+			assemble[strInd] += "<"
+		elif dir == Vector2i.RIGHT:
+			assemble[strInd] += ">"
+	
+	return assemble
+
+
 func solve1(input: String) -> int:
 	var solution: int = 0
 	
@@ -50,7 +66,6 @@ func solve1(input: String) -> int:
 	
 	for rowInd: int in grid.size():
 		for charInd: int in grid[rowInd].size():
-			if unvisitedSpots.has(Vector2i(charInd, rowInd)): continue
 			if grid[rowInd][charInd] == ".":
 				unvisitedSpots.get_or_add(Vector2i(charInd, rowInd), INF)
 			elif grid[rowInd][charInd] == "S":
@@ -60,6 +75,10 @@ func solve1(input: String) -> int:
 				finishPos = Vector2i(charInd, rowInd)
 				unvisitedSpots.get_or_add(Vector2i(charInd, rowInd), INF)
 	
+	var totalSpots: int = unvisitedSpots.size()
+	print(totalSpots)
+	var spotsChecked: int = 0
+	
 	var hasSearchable: bool = true
 	
 	var possiblePaths: Dictionary = {}
@@ -67,7 +86,9 @@ func solve1(input: String) -> int:
 	spotsToVisit.append([startPos, [">"]])
 	
 	while spotsToVisit.size() != 0:
-		
+		print("{percent}%".format({"percent": "%4.2f" % (spotsChecked*100.0/totalSpots)}))
+		print("Spots to check: %d" % spotsToVisit.size())
+		print("Spots checked: %d" % spotsChecked)
 		var focusSpot: Array
 		if spotsToVisit.size() == 1:
 			focusSpot = spotsToVisit[0]
@@ -75,8 +96,8 @@ func solve1(input: String) -> int:
 			focusSpot = spotsToVisit.reduce(func(min, spot): return spot if get_path_length(spot[1][0]) < get_path_length(min[1][0]) else min)
 		
 		#print(spotsToVisit)
-		print(focusSpot)
-		print()
+		#print(focusSpot)
+		#print()
 		
 		if focusSpot[0] == finishPos:
 			for path: String in focusSpot[1]:
@@ -86,37 +107,29 @@ func solve1(input: String) -> int:
 		for yOff: int in range(-1, 2):
 			for xOff: int in range(-1, 2):
 				var offsetSpot: Vector2i = Vector2i(focusSpot[0].x + xOff, focusSpot[0].y + yOff)
-				var dirVec: Vector2i = Vector2i(xOff, yOff)
 				if abs(yOff) != abs(xOff) and ".E".contains(grid[offsetSpot.y][offsetSpot.x]):
-					var visitSpotPaths: Array = focusSpot[1].duplicate()
-					
-					for strInd: int in visitSpotPaths.size():
-						if dirVec == Vector2i.UP:
-							visitSpotPaths[strInd] += "^"
-						elif dirVec == Vector2i.DOWN:
-							visitSpotPaths[strInd] += "v"
-						elif dirVec == Vector2i.LEFT:
-							visitSpotPaths[strInd] += "<"
-						elif dirVec == Vector2i.RIGHT:
-							visitSpotPaths[strInd] += ">"
+					var dirVec: Vector2i = Vector2i(xOff, yOff)
+					var visitSpotPaths: Array
 					
 					if !unvisitedSpots.has(offsetSpot):
-						if get_path_length_no_turn(visitSpotPaths[0]) < get_path_length_no_turn(visitedSpots[offsetSpot][0]):
-							visitedSpots[offsetSpot] = visitSpotPaths
-						elif get_path_length_no_turn(visitSpotPaths[0]) == get_path_length_no_turn(visitedSpots[offsetSpot][0]):
-							visitedSpots[offsetSpot].append_array(visitSpotPaths)
-						else:
-							continue
+						if (focusSpot[1][0]+".").length() < (visitedSpots[offsetSpot][0]).length():
+							visitedSpots[offsetSpot] = increment_path_list(focusSpot[1], dirVec)
+						elif (focusSpot[1][0]+".").length() == (visitedSpots[offsetSpot][0]).length():
+							visitedSpots[offsetSpot].append_array(increment_path_list(focusSpot[1], dirVec))
+						spotsToVisit.erase(focusSpot)
+						continue
 					
-					
+					visitSpotPaths = increment_path_list(focusSpot[1], dirVec)
 					
 					spotsToVisit.append([offsetSpot, visitSpotPaths])
+					
 					
 					if grid[offsetSpot.y][offsetSpot.x] == ".":
 						unvisitedSpots.erase(offsetSpot)
 						visitedSpots.get_or_add(offsetSpot, visitSpotPaths)
 				
 		spotsToVisit.erase(focusSpot)
+		spotsChecked += 1
 	
 	var pathScores: Array[int] = []
 	for path: String in possiblePaths:
