e122bace39c6e3f7c1c81502078f58136901469a
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2024-12-25 10:13:52 -0500

    add 2024 day 24 part 2 progress

--- src/rawSolutions/2024/2024Day24.gd
+++ src/rawSolutions/2024/2024Day24.gd
@@ -76,3 +76,56 @@ func solve1(input: String) -> int:
 	solution = zAssemble.bin_to_int()
 	
 	return solution
+
+
+func solve2(input: String) -> int:
+	var solution: int = 0
+	
+	var registers: Dictionary = {}
+	var instrs: Array[Dictionary] = []
+	
+	var inputSplit: PackedStringArray = input.split("\n\n")
+	var rawInit: PackedStringArray = inputSplit[0].split("\n")
+	var rawInstr: PackedStringArray = inputSplit[1].split("\n")
+	
+	for register: String in rawInit:
+		var regSplit: PackedStringArray = register.split(": ")
+		registers.get_or_add(regSplit[0], regSplit[1].to_int())
+	
+	for instr: String in rawInstr:
+		if instr == "": continue
+		var instrSplit: PackedStringArray = instr.split(" ")
+		
+		instrs.append({
+			"reg1" : instrSplit[0], 
+			"reg2" : instrSplit[2], 
+			"outputReg" : instrSplit[4], 
+			"operation" : instrSplit[1], 
+			})
+	
+	var instrTypes: Array = [[], [], [], [], [], []]
+	
+	for instr: Dictionary in instrs:
+		match instr.operation:
+			"AND":
+				if instr.reg1[2].is_valid_int():
+					instrTypes[0].append(instr)
+				else:
+					instrTypes[1].append(instr)
+			"OR":
+				if instr.reg1[2].is_valid_int():
+					instrTypes[2].append(instr)
+				else:
+					instrTypes[3].append(instr)
+			"XOR":
+				if instr.reg1[2].is_valid_int():
+					instrTypes[4].append(instr)
+				else:
+					instrTypes[5].append(instr)
+	
+	for type: Array in instrTypes:
+		for instr: Dictionary in type:
+			print("%s %s %s -> %s" % [instr.reg1, instr.operation, instr.reg2, instr.outputReg])
+		print()
+	
+	return solution
