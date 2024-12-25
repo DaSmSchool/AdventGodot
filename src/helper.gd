6b7a8e7bc51d0cad1bee617645e2b2d0fb4c6367
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2024-12-25 15:04:51 -0500

    remove typing from grid printing

--- src/helper.gd
+++ src/helper.gd
@@ -16,7 +16,7 @@ static func valid_pos_at_grid(yInd: int, xInd: int, grid: Array) -> bool:
 	return 0 <= yInd and yInd < grid.size() and 0 <= xInd and xInd < grid[0].size()
 
 static func print_grid(grid: Array):
-	for line: Array in grid:
+	for line in grid:
 		var lineAssemble: String = ""
 		for sub in line:
 			lineAssemble += str(sub)
