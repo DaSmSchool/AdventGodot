69be2de8d3d7bf5c55c00ea9da4db4407d9d3269
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2025-03-10 20:57:43 -0400

    add i18n day 4 solution

--- src/rawSolutions/18/18Day4.gd
+++ src/rawSolutions/18/18Day4.gd
@@ -3,6 +3,7 @@ extends Solution
 signal on_request_done
 
 var httpJSON
+var isCached: bool = false
 
 var monthToInt: Dictionary[String, int] = {
 	"Jan": 1,
@@ -12,8 +13,27 @@ var monthToInt: Dictionary[String, int] = {
 
 var apiResponses: Array = []
 
+func check_and_use_cache():
+	if FileAccess.file_exists("res://input/18/D4cache.txt"):
+		var file = FileAccess.open("res://input/18/D4cache.txt", FileAccess.READ)
+		while file.get_position() < file.get_length():
+			var json_string: String = file.get_line()
+
+			var json = JSON.new()
+
+			var parse_result = json.parse(json_string)
+			if not parse_result == OK:
+				print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+				continue
+			apiResponses.append(json.data)
+			
+		isCached = true
+		file.close()
+
 func solve1(input: String) -> int:
 	var solution: int = 0
+	
+	check_and_use_cache()
 	
 	%HTTPRequest.request_completed.connect(_on_request_completed)
 	#%HTTPRequest.request("https://api.github.com/repos/godotengine/godot/releases/latest")
@@ -28,33 +48,39 @@ func solve1(input: String) -> int:
 	print(inputSplit.size())
 	
 	for section: String in inputSplit:
-		print()
-		print(section)
-		print()
+		#print()
+		#print(section)
+		#print()
 		var fromTicketStr: String = section.split("\n")[0]
 		var toTicketStr: String = section.split("\n")[1]
 		var fromTicket: PackedStringArray = fromTicketStr.split(" ", false)
 		var toTicket: PackedStringArray = toTicketStr.split(" ", false)
 		
 		var fromUnix: int = getUnixFromTicket(fromTicket)
-		
 		var toUnix: int = getUnixFromTicket(toTicket)
 		
-		#print("A")
-		await request_tzdb(apiBase + fromTicket[1] + "&time=" + str(fromUnix))
-		#print("B")
-		await on_request_done
-		#print("C")
-		await get_tree().create_timer(1).timeout
-		#print("D")
-		await request_tzdb(apiBase + toTicket[1] + "&time=" + str(toUnix))
-		#print("E")
-		await on_request_done
-		#print("F")
+		if not isCached:
+			#print("A")
+			await request_tzdb(apiBase + fromTicket[1] + "&time=" + str(fromUnix))
+			#print("B")
+			await on_request_done
+			#print("C")
+			await get_tree().create_timer(1).timeout
+			#print("D")
+			await request_tzdb(apiBase + toTicket[1] + "&time=" + str(toUnix))
+			#print("E")
+			await on_request_done
+			#print("F")
+		
 	
 	await apiResponses.size()==inputSplit.size()-1
 	for resp in apiResponses:
-		print(resp)
+		if not isCached:
+			await get_tree().create_timer(0.2).timeout
+			print(JSON.stringify(resp))
+	
+	solution = get_ticket_differences()
+	
 	return solution
 
 func getUnixFromTicket(ticket: PackedStringArray) -> int:
@@ -65,8 +91,24 @@ func getUnixFromTicket(ticket: PackedStringArray) -> int:
 		"hour": ticket[5].split(":")[0].to_int(),
 		"minute": ticket[5].split(":")[1].to_int()
 	}
-	print(assembleDict)
+	#print(assembleDict)
 	return Time.get_unix_time_from_datetime_dict(assembleDict)
+
+func get_ticket_differences() -> int:
+	var sumDiff: int = 0
+	
+	for ticketSetInd: int in range(0, apiResponses.size(), 2):
+		var fromTicket: Dictionary = apiResponses[ticketSetInd]
+		var toTicket: Dictionary = apiResponses[ticketSetInd+1]
+		var time1: int = fromTicket.timestamp - fromTicket.gmtOffset * 2
+		var time2: int = toTicket.timestamp - toTicket.gmtOffset * 2
+		var timeDiff: int = time2 - time1
+		sumDiff += timeDiff
+		#print(Time.get_datetime_string_from_unix_time(time1))
+		#print(Time.get_datetime_string_from_unix_time(time2))
+		print(timeDiff/60)
+	print(sumDiff/60)
+	return sumDiff/60
 
 func request_tzdb(str: String) -> void:
 	await get_tree().create_timer(1.5).timeout
