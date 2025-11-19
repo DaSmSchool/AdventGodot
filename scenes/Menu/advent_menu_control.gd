7c8f76617eaa79168f41345a9519ab5afaf3af0d
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2025-11-19 14:21:48 -0500

    add visualization update

--- scenes/Menu/advent_menu_control.gd
+++ scenes/Menu/advent_menu_control.gd
@@ -2,4 +2,6 @@ extends Node
 class_name AdventMenuControl
 
 func run_raw_advent_solution(path: String) -> void:
+	$VisualizationsMenu.hide()
+	$RawSolutionRunner.show()
 	$RawSolutionRunner.run_solutions(path)
