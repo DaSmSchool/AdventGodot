1426c82ba52849b37572475c8d351dfc7d6e21bb
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2025-12-04 13:54:51 -0500

    add 2023 day 2 part 1

---
+++ scenes/Solutions/2023/Day2/RawSolution/2023Day2.gd
@@ -0,0 +1,48 @@
+extends Solution
+
+var colorLimit: Dictionary = {
+	"red": 12,
+	"green": 13,
+	"blue": 14,
+}
+
+func parse_games(input: String) -> Array[Dictionary]:
+	var gameArray: Array[Dictionary] = []
+	for gameLine: String in input.split("\n", false):
+		var gameSplit: PackedStringArray = gameLine.split(": ")
+		var matchesSplit: PackedStringArray = gameSplit[1].split("; ")
+		var gameDict: Dictionary = {}
+		for gameMatch: String in matchesSplit:
+			var cubesSplit: PackedStringArray = gameMatch.split(", ")
+			for cubeSplit: String in cubesSplit:
+				var amountSplit: PackedStringArray = cubeSplit.split(" ")
+				if not gameDict.has(amountSplit[1]):
+					gameDict[amountSplit[1]] = []
+				gameDict[amountSplit[1]].append(amountSplit[0].to_int())
+		gameArray.append(gameDict)
+	return gameArray
+
+func is_game_valid(game: Dictionary) -> bool:
+	for color: String in colorLimit:
+		if game[color].max() > colorLimit[color]:
+			return false
+	return true
+	
+
+func solve1(input: String) -> Variant:
+	var solution: int = 0
+	
+	var gamesList: Array[Dictionary] = parse_games(input)
+	for gameInd: int in gamesList.size():
+		if is_game_valid(gamesList[gameInd]):
+			solution += gameInd+1
+	
+	
+	return solution
+
+func solve2(input: String) -> Variant:
+	var solution: int = 0
+	
+	
+	
+	return solution
