213d755e7e7330b6cf34ed464886e37e95a31710
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2025-12-07 10:18:06 -0500

    add p2

--- scenes/Solutions/2025/Day7/RawSolution/2025Day7.gd
+++ scenes/Solutions/2025/Day7/RawSolution/2025Day7.gd
@@ -1,9 +1,7 @@
 extends Solution
 
 var splitsMet: Dictionary = {}
-var splitsMetP2: Dictionary = {}
 var visitedSpaces: Dictionary = {}
-var visitedSpacesP2: Dictionary = {}
 
 func make_tachyon_splits(startingPosition: Vector2i, inputSplit: PackedStringArray, tachyonStartingPositions: Dictionary[Vector2i, bool]) -> void:
 	if startingPosition == Vector2i(7, 4):
@@ -23,23 +21,23 @@ func make_tachyon_splits(startingPosition: Vector2i, inputSplit: PackedStringArr
 				make_tachyon_splits(newPosRight, inputSplit, tachyonStartingPositions)
 			break
 
-func make_tachyon_splits_p2(startingPosition: Vector2i, inputSplit: PackedStringArray, tachyonStartingPositions: Dictionary[Vector2i, bool]) -> int:
+func make_tachyon_splits_p2(startingPosition: Vector2i, inputSplit: PackedStringArray, splitsMetP2: Dictionary[Vector2i, int]) -> int:
+	
 	var outcomes: int = 1
 	for rowInd: int in range(startingPosition.y, inputSplit.size()):
 		var focusChar: String = inputSplit[rowInd][startingPosition.x]
 		var position: Vector2i = Vector2i(startingPosition.x, rowInd)
-		visitedSpacesP2[position] = true
 		if focusChar == "^":
 			if splitsMetP2.has(position):
-				outcomes += splitsMetP2[position]-2
-				break
+				return splitsMetP2[position]
 			var newPosLeft: Vector2i = Vector2i(startingPosition.x-1, rowInd)
 			var newPosRight: Vector2i = Vector2i(startingPosition.x+1, rowInd)
-			var newOutcome: int = 0
-			newOutcome += make_tachyon_splits_p2(newPosLeft, inputSplit, tachyonStartingPositions)
-			newOutcome += make_tachyon_splits_p2(newPosRight, inputSplit, tachyonStartingPositions)
-			outcomes = newOutcome
+			var newOutcomeL: int = make_tachyon_splits_p2(newPosLeft, inputSplit, splitsMetP2)
+			var newOutcomeR: int = make_tachyon_splits_p2(newPosRight, inputSplit, splitsMetP2)
+			
+			outcomes += newOutcomeL + newOutcomeR - 1
 			splitsMetP2[position] = outcomes
+			break
 	return outcomes
 
 func print_with_tachyon_paths(grid: PackedStringArray) -> void:
@@ -58,7 +56,7 @@ func print_with_tachyon_paths(grid: PackedStringArray) -> void:
 		gridAssemble += rowAssemble + "\n"
 	print_rich(gridAssemble)
 
-func print_with_tachyon_paths_p2(grid: PackedStringArray) -> void:
+func print_with_tachyon_paths_p2(grid: PackedStringArray, splitsMetP2: Dictionary[Vector2i, int]) -> void:
 	var gridAssemble: String = ""
 	for rowInd: int in grid.size():
 		var rowAssemble: String = ""
@@ -84,7 +82,7 @@ func solve1(input: String) -> Variant:
 	
 	make_tachyon_splits(startingPosition, inputSplit, tachyonStartingPositions)
 	
-	print_with_tachyon_paths(inputSplit)
+	#print_with_tachyon_paths(inputSplit)
 	
 	solution = splitsMet.size()
 	
@@ -96,13 +94,10 @@ func solve2(input: String) -> Variant:
 	var inputSplit: PackedStringArray = input.split("\n", false)
 	var startingPosition: Vector2i = Vector2i(inputSplit[0].find("S"), 0)
 	
-	var tachyonStartingPositions: Dictionary[Vector2i, bool] = {startingPosition:true}
+	var splitsMetP2: Dictionary[Vector2i, int] = {}
 	
-	solution = make_tachyon_splits_p2(startingPosition, inputSplit, tachyonStartingPositions)
-	
-	# this is the split directly below starting point
-	#solution = splitsMetP2[Vector2i(startingPosition.x, 2)]
-	
-	print_with_tachyon_paths_p2(inputSplit)
+	solution = make_tachyon_splits_p2(startingPosition, inputSplit, splitsMetP2)
+
+	#print_with_tachyon_paths_p2(inputSplit, splitsMetP2)
 	
 	return solution
