1065b35cd000e9175699c655a4d2c8406abb54e6
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2025-12-10 00:31:49 -0500

    add 2025 d10 parsing

---
+++ scenes/Solutions/2025/Day10/RawSolution/2025Day10.gd
@@ -0,0 +1,36 @@
+extends Solution
+
+# a few one liner map parsers. as a treat.
+func parse_input(input: String, lightArray: Array, buttonArray: Array, joltageArray: Array) -> void:
+	var lineSplit: PackedStringArray = input.split("\n", false)
+	for line: String in lineSplit:
+		var fieldSplit: PackedStringArray = line.split(" ")
+		lightArray.append(Array(fieldSplit[0].substr(1, fieldSplit[0].length()-2).split("")).map(func(l): return true if l == "#" else false))
+		fieldSplit.remove_at(0)
+		joltageArray.append(Array(fieldSplit[fieldSplit.size()-1].substr(1, fieldSplit[fieldSplit.size()-1].length()-2).split(",")).map(func(j): return int(j)))
+		fieldSplit.remove_at(fieldSplit.size()-1)
+		buttonArray.append(Array(fieldSplit).map(func(b): return Array(b.substr(1, b.length()-1).split(",")).map(func(n): return int(n)))) 
+
+func solve1(input: String) -> Variant:
+	var solution: int = 0
+	
+	var lightArray: Array = []
+	var buttonArray: Array = []
+	var joltageArray: Array = []
+	parse_input(input, lightArray, buttonArray, joltageArray)
+	
+	#for setInd: int in lightArray.size():
+		#print(lightArray[setInd])
+		#print(buttonArray[setInd])
+		#print(joltageArray[setInd])
+		#print()
+	
+	print(buttonArray.map(func(s): return pow(2, s.size())).max())
+	
+	
+	return solution
+
+func solve2(input: String) -> Variant:
+	var solution: int = 0
+	
+	return solution
